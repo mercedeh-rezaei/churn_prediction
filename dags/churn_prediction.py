46abeb93f5cee0971698f988c408f9278db0c6d2
Mercedeh Rezaei <mercedehrezaei@mercedeh.home>
2025-02-05 20:05:30 +0000

    added tasks for training,evaluating and saving the model

--- dags/churn_prediction.py
+++ dags/churn_prediction.py
@@ -2,11 +2,10 @@ import os
 import requests
 from airflow.decorators import dag, task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.providers.postgres.operators.postgres import PostgresOperator
+#from airflow.operators.postgres_operator import PostgresOperator
 from airflow.utils.log.logging_mixin import LoggingMixin
 import datetime
 import pendulum
-
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -35,7 +34,7 @@ default_args = {
 
 @dag(
     dag_id="churn_prediction",
-    schedule_interval="0 0 * * *", # running daily at midnight
+    schedule="0 0 * * *", # running daily at midnight
     start_date=pendulum.datetime(2025, 1, 1, tz='UTC'),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
@@ -108,9 +107,76 @@ def preprocessing(df):
    X_train_processed = pd.DataFrame(X_train_balanced, columns=feature_names)
    X_test_processed = pd.DataFrame(X_test_processed, columns=feature_names)
 
-   return X_train_processed, X_test_processed, y_train_balanced, y_test
+   return X_train_processed, X_test_processed, y_train_balanced, y_test, preprocessor
 
+
+@task 
+def train_model(X_train, y_train):
+    logistic_reg = LogisticRegression(
+        max_iter=1000,
+        random_state=42,
+        class_weight='balanced'
+    )
+
+    logistic_reg.fit(X_train, y_train)
+    return logistic_reg
+
+@task
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test,y_pred)
+
+    class_report = classification_report(y_test, y_pred, output_dict=True)
+
+    # logging the results
+    logger = LoggingMixin().log
+    logger.info(f"Model Accuracy: {accuracy}")
+    logger.info(f"Classification report:\n{classification_report(y_test, y_pred)}")
+    return {
+        'accuracy': accuracy,
+        'classification_report': class_report
+    }
+
+@task
+def save_model(model,preprocessor):
+    # making sure the directory exists
+    os.makedirs('models', exist_ok=True)
+    model_path = 'models/logistic_regression_churn_model.pkl'
+    preprocessor_path = 'models/churn_preprocessor.pkl'
+
+    with open(model_path, 'wb') as f:
+        pickle.dump(model,f)
+
+    with open(preprocessor_path, 'wb') as f:
+        pickle.dump(preprocessor, f)
+
+    logger = LoggingMixin().log
+    logger.info(f"Model saved to {model_path}")
+    logger.info(f"Preprocessor saved to {preprocessor_path}")
+    
+    return model_path, preprocessor_path
+
+
+@task
+def model_workflow(df):
+    X_train, X_test, y_train, y_test, preprocessor = preprocessing(df)
+    print(X_train.haed())
+    print(X_test.head())
+    
+    trained_model = train_model(X_train, y_train)
+    print(trained_model)
+    # Evaluate the model
+    evaluation_results = evaluate_model(trained_model, X_test, y_test)
+    
+    # Save the model and preprocessor
+    model_path, preprocessor_path = save_model(trained_model, preprocessor)
+    
+    return {
+        'evaluation_results': evaluation_results,
+        'model_path': model_path,
+        'preprocessor_path': preprocessor_path
+    }
 
 customer_data = get_data()
-processed_data = preprocessing(customer_data)
-
+workflow_results = model_workflow(customer_data)
