0a839d546bb341eea693ab2f634b62e29b7837fe
Mercedeh Rezaei <mercedehrezaei@mercedeh.home>
2025-02-05 17:07:14 +0000

    added the get_data task and tested it

--- dags/churn_prediction.py
+++ dags/churn_prediction.py
@@ -3,6 +3,7 @@ import requests
 from airflow.decorators import dag, task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.utils.log.logging_mixin import LoggingMixin
 import datetime
 import pendulum
 
@@ -30,6 +31,8 @@ default_args = {
 }
 
 
+
+
 @dag(
     dag_id="churn_prediction",
     schedule_interval="0 0 * * *", # running daily at midnight
@@ -39,7 +42,18 @@ default_args = {
     default_args=default_args,
     tags=['ml', 'churn']
 )
-def churn_prediction_pipeline():
-    pass
 
-churn_prediction_dag = churn_prediction_pipeline()
+@task
+def get_data():
+    postgres_hook = PostgresHook(postgres_conn_id='postgres_churn')
+    query = "SELECT * FROM customers;"
+    df = postgres_hook.get_pandas_df(query)
+    print(f"Data shape: {df.shape}")
+    print(f"Data Head: {df.head()}")
+    print(f"Data info: {df.info()}")
+    logger = LoggingMixin().log
+    logger.info(f"Data shape: {df.shape}")
+    logger.info(f"\nData Head: \n{df.head()}")
+    return df 
+
+customer_data = get_data()
