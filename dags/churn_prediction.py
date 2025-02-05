7ba3fb3cfb1b8d69832c9259bd2813afba9fb4c4
Mercedeh Rezaei <mercedehrezaei@mercedeh.home>
2025-02-05 19:29:40 +0000

    added the preprocessing task

--- dags/churn_prediction.py
+++ dags/churn_prediction.py
@@ -56,4 +56,61 @@ def get_data():
     logger.info(f"\nData Head: \n{df.head()}")
     return df 
 
+
+@task
+def preprocessing(df):
+   df = df.copy()
+   
+   features = ['age', 'contract_length', 'monthly_charges', 'contract_type']
+   target = 'churn'
+   
+   X = df[features]
+   y = df[target]
+   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+   
+   numerical_features = ['age', 'contract_length', 'monthly_charges']
+   categorical_features = ['contract_type']
+
+    # pipeline for numerical features, imputing null values and scaling
+   numeric_transformer = Pipeline(steps=[
+       ('imputer', SimpleImputer(strategy='median')),
+       ('scaler', StandardScaler())
+   ])
+    # pipeline for categorical features, imputing null values and one hot encoding contract type
+   categorical_transformer = Pipeline(steps=[
+       ('imputer', SimpleImputer(strategy='most_frequent')),
+       ('onehot', OneHotEncoder(drop='first', sparse_output=False))
+   ])
+
+   preprocessor = ColumnTransformer(
+       transformers=[
+           ('num', numeric_transformer, numerical_features),
+           ('cat', categorical_transformer, categorical_features)
+       ])
+   
+
+   X_train_processed = preprocessor.fit_transform(X_train)
+   
+   # calling .transform instead of .fit_transform on the test set to prevent data leakage
+   X_test_processed = preprocessor.transform(X_test)
+
+    # using SMOTE for class imbalance
+   smote = SMOTE(random_state=42)
+   X_train_balanced, y_train_balanced = smote.fit_resample(X_train_processed, y_train)
+
+   numeric_features = numerical_features
+   categorical_features_encoded = [f"contract_type_{cat}" for cat in 
+                                 preprocessor.named_transformers_['cat']
+                                 .named_steps['onehot']
+                                 .get_feature_names_out(['contract_type'])]
+   feature_names = numeric_features + list(categorical_features_encoded)
+
+   X_train_processed = pd.DataFrame(X_train_balanced, columns=feature_names)
+   X_test_processed = pd.DataFrame(X_test_processed, columns=feature_names)
+
+   return X_train_processed, X_test_processed, y_train_balanced, y_test
+
+
 customer_data = get_data()
+processed_data = preprocessing(customer_data)
+
